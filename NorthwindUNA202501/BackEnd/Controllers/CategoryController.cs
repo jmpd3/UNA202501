8fdfffefde5b0323ab53d480a118f49bbcdbb1da
juanpabloramosp@hotmail.com <juanpabloramosp@hotmail.com>
2025-03-07 20:05:15 -0600

    CRUD Category Completed

--- NorthwindUNA202501/BackEnd/Controllers/CategoryController.cs
+++ NorthwindUNA202501/BackEnd/Controllers/CategoryController.cs
@@ -1,7 +1,7 @@
 ﻿using BackEnd.DTO;
 using BackEnd.Services.Interfaces;
-using DAL.Interfaces;
-using Entities.Entities;
+
+
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,28 +26,31 @@ namespace BackEnd.Controllers
 
         // GET api/<CategoryController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public CategoryDTO Get(int id)
         {
-            return "value";
+            return _categoryService.GetCategoryById(id);
         }
 
         // POST api/<CategoryController>
         [HttpPost]
-        public void Post([FromBody]Category category)
+        public void Post([FromBody]CategoryDTO category)
         {
-           
+            _categoryService.AddCategory(category);
+
         }
 
         // PUT api/<CategoryController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        public void Put([FromBody]CategoryDTO category)
         {
+            _categoryService.UpdateCategory(category);
         }
 
         // DELETE api/<CategoryController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _categoryService.DeleteCategory(id);
         }
     }
 }
--- NorthwindUNA202501/BackEnd/Services/Implementations/CategoryService.cs
+++ NorthwindUNA202501/BackEnd/Services/Implementations/CategoryService.cs
@@ -1,6 +1,7 @@
 ﻿using BackEnd.DTO;
 using BackEnd.Services.Interfaces;
 using DAL.Interfaces;
+using Entities.Entities;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace BackEnd.Services.Implementations
@@ -13,19 +14,68 @@ namespace BackEnd.Services.Implementations
         {
             _unidadDeTrabajo = unidad;
         }
+
+        CategoryDTO Convertir(Category category)
+        {
+            return new CategoryDTO
+            {
+                CategoryId = category.CategoryId,
+                CategoryName = category.CategoryName
+            };
+        } 
+        
+        Category Convertir(CategoryDTO category)
+        {
+            return new Category
+            {
+                CategoryId = category.CategoryId,
+                CategoryName = category.CategoryName
+            };
+        }
+
+        public CategoryDTO AddCategory(CategoryDTO category)
+        {
+
+            
+            _unidadDeTrabajo.CategoryDAL.Add(Convertir(category));
+            _unidadDeTrabajo.Complete();
+            return category;
+        }
+
+        public CategoryDTO DeleteCategory(int id)
+        {
+            var Category = new Category { CategoryId = id };
+            _unidadDeTrabajo.CategoryDAL.Remove(Category);
+            _unidadDeTrabajo.Complete();
+            return Convertir(Category);
+        }
+
         public List<CategoryDTO> GetCategories()
         {
             var categories = _unidadDeTrabajo.CategoryDAL.Get(); 
             List<CategoryDTO> categoryDTOs = new List<CategoryDTO>();
             foreach (var category in categories)
             {
-                categoryDTOs.Add(new CategoryDTO
-                {
-                    CategoryId = category.CategoryId,
-                    CategoryName = category.CategoryName
-                }); 
+                categoryDTOs.Add(this.Convertir(category)); 
             }
             return categoryDTOs;
+        }
+
+        public CategoryDTO GetCategoryById(int id)
+        {
+            var result = _unidadDeTrabajo.CategoryDAL.FindById(id);
+            return Convertir(result);
+        }
+
+        public CategoryDTO UpdateCategory(CategoryDTO category)
+        {
+            var entity = Convertir(category);
+            _unidadDeTrabajo.CategoryDAL.Update(entity);
+            _unidadDeTrabajo.Complete();
+
+          
+
+            return category;
         }
     }
 }
